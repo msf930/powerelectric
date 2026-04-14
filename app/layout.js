7d8febc72a970d77c2f2a8a00171dabb9c2e730f
Matt Folsom <msf930@gmail.com>
2026-04-14 12:33:48 -0600

    clarity final

--- app/layout.js
+++ app/layout.js
@@ -94,7 +94,7 @@ export default function RootLayout({ children }) {
             id="microsoft-clarity"
             strategy="afterInteractive"
             dangerouslySetInnerHTML={{
-              __html: `(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", ${JSON.stringify(clarityProjectId)});`,
+              __html: `(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "wbop021nwj");`,
             }}
           />
         
