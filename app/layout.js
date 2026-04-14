87ad3e6ba4a640e5b41f6a062e0ab10a3f727aee
Matt Folsom <msf930@gmail.com>
2026-04-14 12:31:25 -0600

    update clarity

--- app/layout.js
+++ app/layout.js
@@ -89,15 +89,16 @@ export default function RootLayout({ children }) {
             __html: JSON.stringify(jsonLd),
           }}
         />
-        {clarityProjectId ? (
+        
           <Script
             id="microsoft-clarity"
             strategy="afterInteractive"
             dangerouslySetInnerHTML={{
-              __html: `(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", wbop021nwj);`,
+              __html: `(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", ${JSON.stringify(clarityProjectId)});`,
             }}
           />
-        ) : null}
+        
+        
         {children}
       </body>
     </html>
